5f74bb7d50f922d37446d179cc41553e692331e5
Michelle Un <michelleun@gmail.com>
2022-11-16 18:40:01 -0500

    reverting

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const express = require("express");
-var bodyParser = require("body-parser");
-const methodOverride = require("method-override");
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(methodOverride("_method"));
 
 const { People, Place, Thing, Souvenir, conn } = require("./db");
 
@@ -22,13 +16,6 @@ app.get("/", async (req, res, next) => {
       return items
         .map((item) => {
           return `<li>${item.name}</li>`;
-        })
-        .join("");
-    };
-    const displayDropDown = (items) => {
-      return items
-        .map((item) => {
-          return `<option value = ${item.id}>${item.name}</option>`;
         })
         .join("");
     };
@@ -51,57 +38,11 @@ app.get("/", async (req, res, next) => {
     <ul>
     ${displayItems(things)}
       </ul>
-      <h2>Souvenir Purchases</h2>
-            <h3>Create a new Souvenir Purchase by selecting a Person, the Place they purchased the souvenir, and the Thing they bought</h3>
-      <form method="POST" action="/">
-      <select name="personId">
-         ${displayDropDown(people)}
-      </select>
-        <select name="placeId">
-          ${displayDropDown(places)}
-      </select>
-    <select name="thingId">
-      ${displayDropDown(things)} 
-      </select>
-      <button>Create</button>
-      </form>
-      <ul>
-      ${souvenirs
-        .map((souvenir) => {
-          return `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} in ${souvenir.place.name}</li>
-          <form method="POST" action="/${souvenir.id}?_method=DELETE"><button>Delete</button></form>
-          `;
-        })
-        .join("")}
-      </ul>
+     
   </body>
   </html>
   `;
     res.send(html);
-  } catch (er) {
-    next(er);
-  }
-});
-
-app.post("/", async (req, res, next) => {
-  try {
-    await Souvenir.create({
-      personId: req.body.personId,
-      placeId: req.body.placeId,
-      thingId: req.body.thingId,
-    });
-    res.redirect("/");
-  } catch (er) {
-    next(er);
-  }
-});
-
-app.delete("/:id", async (req, res, next) => {
-  try {
-    await Souvenir.destroy({
-      where: { id: req.params.id },
-    });
-    res.redirect("/");
   } catch (er) {
     next(er);
   }
